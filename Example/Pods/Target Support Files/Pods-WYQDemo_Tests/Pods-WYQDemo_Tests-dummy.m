5f83fe28d1b0fdf5219a2f073f58cf349004df84
923369580@qq.com <923369580@qq.com>
2017-08-15 11:12:32 +0800

    Initial commit of library
    Signed-off-by: 923369580@qq.com <923369580@qq.com>

---
+++ Example/Pods/Target Support Files/Pods-WYQDemo_Tests/Pods-WYQDemo_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WYQDemo_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_WYQDemo_Tests
+@end
---
+++ Example/Pods/Target Support Files/WYQDemo/WYQDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WYQDemo : NSObject
+@end
+@implementation PodsDummy_WYQDemo
+@end
